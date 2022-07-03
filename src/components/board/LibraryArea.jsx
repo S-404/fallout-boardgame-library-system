70e236e92905f6f81ce40f463be83f8f740fb25f
Andrey <sudarev.andrey.i@gmail.com>
2022-07-03 11:42:12 +0300

    upd LibraryArea.jsx add library menu

--- src/components/board/LibraryArea.jsx
+++ src/components/board/LibraryArea.jsx
@@ -3,16 +3,37 @@ import Deck from "../deck/Deck";
 import questImg from '../../assets/questCardBack.png'
 
 const LibraryArea = () => {
-    return (
-        <div>
-            <div>
-                <Deck
-                name={'Quests'}
-                backImg={questImg}
-                menu={''}
+
+    const [cardNum, setCardNum] = useState(0)
+
+    const onInputChange = (e) => {
+        setCardNum(e.target.value)
+    }
+
+    const libraryMenu = () => {
+        return (
+            <div className='menu__menu-inputs'>
+                <input
+                    className='menu-inputs__input'
+                    type='number'
+                    value={cardNum}
+                    onChange={onInputChange}
                 />
+                <div className='menu-inputs__buttons'>
+                    <button>Add</button>
+                    <button>Stage</button>
+                </div>
+
             </div>
-        </div>
+        )
+    }
+
+    return (
+        <Deck
+            name={'Quests'}
+            backImg={questImg}
+            menu={libraryMenu()}
+        />
     );
 };
 
