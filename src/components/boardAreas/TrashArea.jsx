d219c4e32e79c77a2e80ad52867b41c8cd798f3b
andrey_s <sudarev.andrey.i@gmail.com>
2022-07-09 11:43:46 +0300

    upd TrashArea.jsx

--- src/components/boardAreas/TrashArea.jsx
+++ src/components/boardAreas/TrashArea.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Context} from "../../store/context";
+import MyAccordion from "../UI/myAccordion/MyAccordion";
 
 const TrashArea = () => {
+    const {trash} = useContext(Context)
+
+    const trashList = () => {
+        return (
+            <div>
+                {trash.map(card => (
+                        <div key={card.id}>{card.num}</div>
+                    ))}
+            </div>
+        );
+    }
+
+
     return (
         <div>
-            <div>trash </div>
+
+            <MyAccordion
+                items={[{
+                    id: 1,
+                    title: `trash (${trash.length})`,
+                    content:trashList()
+                }]}/>
+
         </div>
     );
 };
