cb6aed19f6d707a5efd7f0488318fe61ad21745e
andrey_s <sudarev.andrey.i@gmail.com>
2022-07-09 11:58:44 +0300

    decomposition QuestCardsArea.jsx

--- src/components/boardAreas/questCardsArea/QuestCardsArea.jsx
+++ src/components/boardAreas/questCardsArea/QuestCardsArea.jsx
@@ -1,59 +1,19 @@
-import React, {useState, useContext} from 'react';
+import React, {useContext} from 'react';
 import Deck from "../../deck/Deck";
 import questImg from '../../../assets/questCardBack.png'
 
 import {Context} from "../../../store/context";
+import QuestCardsMenu from "./QuestCardsMenu";
 
 const QuestCardsArea = () => {
 
-    const [cardNum, setCardNum] = useState('000')
-    const [selectedCard, setSelectedCard] = useState(null)
-    const {questsDeck, addCardFrom, stageCardFrom} = useContext(Context)
-
-    const onInputChange = (e) => {
-        setCardNum(e.target.value)
-        const cardIndex = questsDeck.findIndex(card => card.num === e.target.value.padStart(3,'0'))
-        setSelectedCard(cardIndex !== -1 ? questsDeck[cardIndex] : null)
-    }
-
-    const stageButtonHandler = () => {
-        stageCardFrom(selectedCard, questsDeck)
-    }
-
-    const addButtonHandler = () => {
-        addCardFrom(selectedCard,questsDeck)
-    }
-
-
-    const questCardsMenu = () => {
-        return (
-            <div className='menu__menu-inputs'>
-                <input
-                    className='menu-inputs__input'
-                    placeholder='card num'
-                    maxLength='3'
-                    value={cardNum}
-                    onChange={onInputChange}
-                />
-                <div className='menu-inputs__buttons'>
-                    {selectedCard === null ? 'not found' :
-                        selectedCard?.type === 'quest' ?
-                            <button onClick={stageButtonHandler}>Stage</button> :
-
-                            <button onClick={addButtonHandler}>Add</button>
-                    }
-
-                </div>
-
-            </div>
-        )
-    }
+    const {questsDeck} = useContext(Context)
 
     return (
         <Deck
             name={'Quests'}
             backImg={questImg}
-            menu={questCardsMenu()}
+            menu={<QuestCardsMenu/>}
             deck={questsDeck}
         />
     );
---
+++ src/components/boardAreas/questCardsArea/QuestCardsMenu.jsx
@@ -0,0 +1,52 @@
+import React,{useContext,useState}  from 'react';
+import {Context} from "../../../store/context";
+
+const QuestCardsMenu = () => {
+
+    const [cardNum, setCardNum] = useState('000')
+    const [selectedCard, setSelectedCard] = useState(null)
+    const {questsDeck, addCardFrom, stageCardFrom} = useContext(Context)
+
+
+    const onInputChange = (e) => {
+        setCardNum(e.target.value)
+        const cardIndex = questsDeck.findIndex(card => card.num === e.target.value.padStart(3,'0'))
+        setSelectedCard(cardIndex !== -1 ? questsDeck[cardIndex] : null)
+    }
+
+    const stageButtonHandler = () => {
+        stageCardFrom(selectedCard, questsDeck)
+    }
+
+    const addButtonHandler = () => {
+        addCardFrom(selectedCard, questsDeck)
+    }
+
+    return (
+        <div className='menu__menu-inputs'>
+
+            <input
+                className='menu-inputs__input'
+                placeholder='card num'
+                maxLength='3'
+                value={cardNum}
+                onChange={onInputChange}
+            />
+
+            <div className='menu-inputs__buttons'>
+
+                {selectedCard === null ?
+                    'not found' :
+                    selectedCard?.type === 'quest' ?
+                        <button onClick={stageButtonHandler}>Stage</button>
+                        :
+                        <button onClick={addButtonHandler}>Add</button>
+                }
+
+            </div>
+
+        </div>
+    )
+};
+
+export default QuestCardsMenu;
