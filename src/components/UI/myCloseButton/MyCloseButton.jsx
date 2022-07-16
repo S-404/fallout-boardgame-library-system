88c687c31c1f77cea7f6fa5f2b26d9c15c577f85
andrey_s <sudarev.andrey.i@gmail.com>
2022-07-16 14:35:32 +0300

    init ui myCloseButton

---
+++ src/components/UI/myCloseButton/MyCloseButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './myCloseButton.css'
+
+const MyCloseButton = () => {
+    return (
+        <div className="myCloseButton"/>
+    );
+};
+
+export default MyCloseButton;
