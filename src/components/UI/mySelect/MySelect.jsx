57e3a2fc891010ea60bf0070807db13e6d635cce
andrey_s <sudarev.andrey.i@gmail.com>
2022-08-21 18:20:50 +0300

    upd Marker.jsx

--- src/components/UI/mySelect/MySelect.jsx
+++ src/components/UI/mySelect/MySelect.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const MySelect = ({title, options, ...props}) => {
     return (
-        <div>
-            {title??<label htmlFor={title}>{title}</label>}
 
-            <select name={title} id={title} {...props}>
-                {options.map(option=>(
-                    <option
+        <FormControl variant="standard" size='small'>
+            {title ? <InputLabel id={`${title}-select-standard-label`}>{title}</InputLabel> : null}
+            <Select
+                labelId={`${title}-select-standard-label`}
+                id={`${title}-select-standard`}
+                label={title}
+                name={title}
+                {...props}
+            >
+                {options.map(option => (
+                    <MenuItem
                         key={`select_option_${option.value}`}
                         value={option.value}
                     >
                         {option.name}
-                    </option>
+                    </MenuItem>
                 ))}
-            </select>
-        </div>
+            </Select>
+        </FormControl>
     );
 };
 
--- src/components/card/cardMenu/Marker.jsx
+++ src/components/card/cardMenu/Marker.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
-import MySelect from "../../UI/mySelect/MySelect";
+import * as React from 'react';
+import {useState} from 'react';
+import {styled} from '@mui/material/styles';
+import Tooltip, {tooltipClasses} from '@mui/material/Tooltip';
+import CircleIcon from '@mui/icons-material/Circle';
+import IconButton from "@mui/material/IconButton";
 
-const Marker = () => {
+const PickColorsTooltip = styled(({className, ...props}) => (
+    <Tooltip {...props} classes={{popper: className}}/>
+))(({theme}) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: '#f5f5f9',
+        color: 'rgba(0, 0, 0, 0.87)',
+        maxWidth: 220,
+        fontSize: theme.typography.pxToRem(12),
+        border: '1px solid #dadde9',
+    },
+}));
 
-    const markerOptions = [
-        {name:'marker',value:''},
-        {name:'red',value:'red'},
-        {name:'green',value:'green'},
-        {name:'blue',value:'blue'},
-    ]
+export default function Marker() {
+
+    const [markerColor, setMarkerColor] = useState('gray')
+    const colors = ['red', 'green', 'blue', 'orange', 'gray']
 
     return (
         <div>
-           <MySelect
-               options={markerOptions}
-           />
+            <PickColorsTooltip
+                enterDelay={500} leaveDelay={200}
+                title={
+                    <>
+                        {colors.map(color => (
+                            <IconButton
+                                key={`marker_color_${color}`}
+                                onClick={() => setMarkerColor(color)}
+                            >
+                                <CircleIcon sx={{color}}/>
+                            </IconButton>
+
+                        ))}
+                    </>
+                }
+            >
+                <IconButton
+                    size="small"
+                >
+                    <CircleIcon
+                        fontSize="inherit"
+                        sx={{color: markerColor}}
+                    />
+                </IconButton>
+            </PickColorsTooltip>
         </div>
     );
-};
-
-export default Marker;
+}
