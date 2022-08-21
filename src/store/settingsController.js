356ed10b6fbbbfd62e34d2e5ae4f88e0b61a645f
andrey_s <sudarev.andrey.i@gmail.com>
2022-08-21 15:05:58 +0300

    upd store - settingsController.js

--- src/store/settingsController.js
+++ src/store/settingsController.js
@@ -4,6 +4,8 @@ import {makeAutoObservable} from "mobx";
 class SettingsController {
 
     playersQty = 2
+    scenario = 0
+    expansion1 = true
     settingsModal = true
 
     constructor() {
@@ -16,6 +18,12 @@ class SettingsController {
 
     setPlayersQty = (newValue) => {
         this.playersQty = newValue
+    }
+    setScenario = (newValue) => {
+        this.scenario = newValue
+    }
+    setExpansion1 = () => {
+        this.expansion1 = !this.expansion1
     }
 }
 
