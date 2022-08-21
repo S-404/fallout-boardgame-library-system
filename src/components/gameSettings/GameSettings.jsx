494d2893ae1d1f3ef7504f65113c6c2fc2feec9f
andrey_s <sudarev.andrey.i@gmail.com>
2022-08-21 15:11:19 +0300

    upd GameSettings.jsx - add input handlers

--- src/components/gameSettings/GameSettings.jsx
+++ src/components/gameSettings/GameSettings.jsx
@@ -1,41 +1,74 @@
 import React from 'react';
-import MyInput from "../UI/myInput/MyInput";
 import MySelect from "../UI/mySelect/MySelect";
-import CardsController from "../../store/cardsController";
 import {observer} from "mobx-react-lite";
+import SettingsController from "../../store/settingsController";
+import CardsController from "../../store/cardsController";
 
-const GameSettings = observer( () => {
+const GameSettings = observer(() => {
 
-    const {playersQty, setPlayersQty} = CardsController
+    const {playersQty, setPlayersQty, scenario, setScenario, expansion1, setExpansion1, setSettingsModal} = SettingsController
+    const {startScenario} = CardsController
 
-    const inputPlayersQtyHandler = (e) => {
-        const newValue = +e.target.value
-        if (newValue >= 1 && newValue <= 4)
-            setPlayersQty(newValue)
+    const selectScenarioHandler = (e) => {
+        setScenario(e.target.value)
+
+    }
+    const selectPlayersQtyHandler = (e) => {
+        console.log(e.target.value)
+        setPlayersQty(e.target.value)
     }
 
-    const scenarios = [
-        {name: 'first', value: 'first'},
-        {name: 'second', value: 'second'},
-        {name: 'third', value: 'third'},
-        {name: 'fourth', value: 'fourth'},
+    const buttonStartHandler = () => {
+        console.log('start scenario ' + scenario + ', for ' + playersQty + ' players')
+        startScenario({
+            scenario,
+            withExpansion1: expansion1
+        })
+        setSettingsModal(false)
+    }
+
+    const scenariosOptions = [
+        {name: 'first', value: 0},
+        {name: 'second', value: 1},
+        {name: 'third', value: 2},
+        {name: 'fourth', value: 3}
+    ]
+
+    const playersQtyOptions = [
+        {name: '1 player', value: 1},
+        {name: '2 players', value: 2},
+        {name: '3 players', value: 3},
+        {name: '4 players', value: 4}
     ]
 
     return (
         <div>
             <div>
-                <MyInput
-                    type='number'
-                    min={1}
-                    max={4}
+                <MySelect
+                    title={'Players Qty'}
+                    options={playersQtyOptions}
+                    onChange={selectPlayersQtyHandler}
                     value={playersQty}
-                    onChange={inputPlayersQtyHandler}
-                    labeltext={'players qty'}
                 />
                 <MySelect
                     title={'Scenario'}
-                    options={scenarios}
+                    options={scenariosOptions}
+                    onChange={selectScenarioHandler}
+                    value={scenario}
                 />
+                <div>
+                    <label>with expansion</label>
+                    <input
+                        type='checkbox'
+                        checked={expansion1}
+                        onChange={setExpansion1}
+                    />
+
+                </div>
+                <button
+                    onClick={buttonStartHandler}
+                >Start
+                </button>
             </div>
         </div>
     );
